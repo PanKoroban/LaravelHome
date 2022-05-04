f344b4da13d49bc78e24e27f41b6ebdee2483659
PanKoroban <75024566+PanKoroban@users.noreply.github.com>
2022-05-04 23:47:04 +0300

    lesson3

---
+++ resources/views/allnews.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.main')
+@section('content')
+    <div class="album py-5 bg-light">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+@foreach ($news as $n)
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                            <div class="card-body">
+
+        <a href="http://localhost/news/{{$n['id']}}">
+            <button type="button" class="btn btn-sm btn-outline-secondary">
+            № {{$n['id']}} {{$n['name']}}
+            </button>
+        </a>
+                                @foreach ($cat as $c)
+                                @if ($c['id'] == $n['category_id'])
+                                        <a href="{{asset('/category/')}}/{{$c['id']}}">
+                                <button type="button" class="btn btn-secondary"> Категория: {{$c['category']}}</button>
+                                        </a>
+                                    @endif
+                                @endforeach
+
+        <p class="card-text">{{$n['text']}}</p>
+
+                            </div>
+                        </div>
+                    </div>
+@endforeach
+            </div>
+        </div>
+    </div>
+@endsection
